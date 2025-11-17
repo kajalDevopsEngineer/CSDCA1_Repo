e9f342e3f7bacb8010b8aa794e97198bb6383e87
kajalDevopsEngineer <kajaldevopsudemy@gmail.com>
2025-11-17 18:18:16 +0000

    Added Unit Tests

---
+++ BPCalculatorUnit.Tests/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using Xunit;
+using BPCalculator;
+
+namespace BPCalculatorUnit.Tests
+{
+    public class BloodPressureCategoryTests
+    {
+        [Fact]
+        public void Category_IsHigh_When_Systolic_AtLeast_140()
+        {
+            // Arrange
+            var bp = new BloodPressure { Systolic = 140, Diastolic = 70 };
+
+            // Act
+            var category = bp.Category;
+
+            // Assert
+            Assert.Equal(BPCategory.High, category);
+        }
+
+        [Fact]
+        public void Category_IsHigh_When_Diastolic_AtLeast_90()
+        {
+            var bp = new BloodPressure { Systolic = 120, Diastolic = 90 };
+
+            var category = bp.Category;
+
+            Assert.Equal(BPCategory.High, category);
+        }
+
+        [Fact]
+        public void Category_IsPreHigh_When_Systolic_Between_120_And_139()
+        {
+            var bp = new BloodPressure { Systolic = 130, Diastolic = 70 };
+
+            var category = bp.Category;
+
+            Assert.Equal(BPCategory.PreHigh, category);
+        }
+
+        [Fact]
+        public void Category_IsPreHigh_When_Diastolic_Between_80_And_89()
+        {
+            var bp = new BloodPressure { Systolic = 110, Diastolic = 85 };
+
+            var category = bp.Category;
+
+            Assert.Equal(BPCategory.PreHigh, category);
+        }
+
+        [Fact]
+        public void Category_IsIdeal_When_Within_Ideal_Range()
+        {
+            var bp = new BloodPressure { Systolic = 110, Diastolic = 70 };
+
+            var category = bp.Category;
+
+            Assert.Equal(BPCategory.Ideal, category);
+        }
+
+        [Fact]
+        public void Category_IsLow_When_Below_Ideal_Range()
+        {
+            var bp = new BloodPressure { Systolic = 85, Diastolic = 55 };
+
+            var category = bp.Category;
+
+            Assert.Equal(BPCategory.Low, category);
+        }
+    }
+}
